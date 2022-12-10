284f478327eff884e08c72b0c1c9dfc6401bdac9
pratikk2609 <pratikpawar2609@gmail.com>
2022-12-10 20:37:29 +0530

    python

---
+++ Fahrenheit to celcuis.py
@@ -0,0 +1,4 @@
+tempF=float(input("enter the temp in farenheit:"))
+tempC=5*(tempF-32)/9
+print("Temperature in farenheit: ", round(tempF, 3), "°F")
+print("Temperature in celcius: ", round(tempC, 2), "°C")
