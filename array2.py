f952ed2be9125aa7b21ed32ac94c59126849e7a4
pratikk2609 <pratikpawar2609@gmail.com>
2022-12-12 20:11:58 +0530

    array 2

---
+++ array2.py
@@ -0,0 +1,10 @@
+from array import *
+array_num = array('i', [1,3,5,7,9])
+for i in array_num:
+    print(i)
+print("accessing one by one")
+print(array_num[0])
+print(array_num[1])
+print(array_num[2])
+print(array_num[3])
+print(array_num[4])
