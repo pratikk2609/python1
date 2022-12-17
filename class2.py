7ec2ce81980fca643b538850bce598cb006b95a0
pratikk2609 <pratikpawar2609@gmail.com>
2022-12-17 11:40:02 +0530

    class string list

---
+++ class2.py
@@ -0,0 +1,111 @@
+'''
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+
+print(a/b)
+print(a//b)
+'''
+
+'''
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+
+print((a>=b) or (a==b))
+'''
+
+'''
+a=60
+print(a>>2)
+'''
+
+'''
+l1=["Abc", "xyz"]
+ print("pqr" not in l1)
+'''
+
+'''
+l1=["Abc", "xyz"]
+print("pqr" not in l1)
+'''
+
+'''
+l1=["Abc", "xyz"]
+l2=["Abc", "xyz"]
+
+l3=l1
+
+print(l3 is l1)
+'''
+
+'''
+str1="abcdef"
+print(str1)
+
+
+l1=[10,20, "ABC", 22, 33]
+print(l1)
+
+l1=(10,20, "ABC", 22, 33)
+print(l1)
+
+s1={10,20, "ABC", 22, 33}
+print(s1)
+
+d1={1000:34.9, "Roll no":123 , "College": "VIIT"}
+print(d1)
+
+'''
+
+'''
+str1="abcde"
+print(str1)
+print(str1[3])
+print(str1[-1])
+
+'''
+
+'''
+s1={10,20, "ABC", 22, 33}
+print(s1)
+print(s1[2])
+'''
+
+'''
+d1={1000:34.9, "Roll no":123 , "College": "VIIT"}
+print(d1)
+print(d1[1000])
+print(d1["Roll no"])
+print(d1["College"])
+'''
+
+'''
+s1={10,20, "ABC", 22, 33, 10,20, "ABC", 22, 33}
+print(s1)
+'''
+
+'''
+str2="ABCDEFGHIJK"
+print(str2)
+print(str2[2:6])
+print(str2[-9:-5])
+'''
+
+'''
+str2="ABCDEFGHIJK"
+print(str2[1:10:2])
+print(str2[1:10:3])
+print(str2[1::2])
+print(str2[1:10:-2])
+print(str2[3::-2])
+print(str2[-1:-10:-2])
+'''
+
+'''
+str2="ABCDEFGHIJK"
+del str2[0]
+'''
+
+'''
+t2=(10,20,30,40,50)
+t2[-1]="A"
+'''
