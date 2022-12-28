369d151af5c7d0be6305699f56a27bcfaac1e023
pratikk2609 <pratikpawar2609@gmail.com>
2022-12-28 21:13:23 +0530

    dictionary

---
+++ class5.py
@@ -0,0 +1,8 @@
+Test=[{'Arizona':'Phoenix','California':'Sacramentro','Hawaii':'Honolulu'}
+,1000,2000,3000,['hat','t-shirt','jeans',{'socks1':'red','socks2':'blue'}]]
+print(Test[2])
+print(Test[0])
+print(Test[4])
+print(Test[0]['Arizona'])
+print(Test[4][2])
+print(Test[4][3]['socks2'])
