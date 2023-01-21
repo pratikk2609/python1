e9cdd5f70af99c7e0dc61e2ef668ac2aa1f380e2
pratikk2609 <pratikpawar2609@gmail.com>
2023-01-21 21:17:00 +0530

    class work 21 2 23

---
+++ classwork21/01.py
@@ -0,0 +1,25 @@
+'''
+x=lambda a:a+10
+print(x(5))
+'''
+
+'''
+square=lambda x:x**2
+print(square(12))
+'''
+
+'''
+x=lambda a,b:a*b
+print(x(5,6))
+'''
+
+'''
+import math
+print(math.sin(90))
+print(math.pi)
+'''
+
+
+import math as M
+print(M.sin(90))
+print(M.pi)
