2ffc78cf3edea571777dfe9775de31cb22388a35
pratikk2609 <pratikpawar2609@gmail.com>
2023-01-29 21:05:55 +0530

    numpy

---
+++ class28-01.py
@@ -0,0 +1,52 @@
+'''
+import numpy
+
+A0=numpy.array([10,20,30])
+print(A0)
+'''
+
+'''
+import numpy
+
+A0=numpy.array([10,20,30])
+print(A0[2])
+'''
+
+
+'''
+import numpy
+
+A1=numpy.array([10,20,30,"ABC"])
+print(A1)
+print("data type of given array=", A1.dtype)
+'''
+
+'''
+import numpy
+
+A0=numpy.array([10,2.0,3.3])
+print(A0)
+print("data type of given array=", A0.dtype)
+'''
+
+
+'''
+import numpy
+
+A0=numpy.array([10,20,30,40])
+print("1d array=",A0)
+print("dimention of given array=", A0.ndim)
+print("shape of given array=", A0.shape)
+print("data type of given array=", A0.dtype)
+'''
+
+
+'''
+import numpy
+
+A1=numpy.array([[10,20,30],[40,50,60],[70,80,90]])
+print("2d array=\n",A1)
+print("dimention of given array=", A1.ndim)
+print("shape of given array=", A1.shape)
+print("data type of given array=", A1.dtype)
+'''
